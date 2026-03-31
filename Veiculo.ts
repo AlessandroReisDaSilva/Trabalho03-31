fe8a45594fc47d81e97fb53ecce01ac1e83549fe
Rafael Pereira <rafaelairespereira07@gmail.com>
2026-03-31 12:46:48 -0300

    Ajuste nos erros de merge com a branch DEV e adicionada a função de descer marcha

--- Veiculo.ts
+++ Veiculo.ts
@@ -1,11 +1,11 @@
 export class Veiculo {
-    marca: string = 'Padrão';
-    modelo: string = 'Padrão';
-    potencia: number = 0;
-    rodas: number = 0;
-    numeroMarchas: number = 5;
-    marchaAtual: number = 0;
-    velocidade: number = 0;
-    nome: string = 'jon';
-    ano: number = 0;
+  marca: string = "Padrão";
+  modelo: string = "Padrão";
+  potencia: number = 0;
+  rodas: number = 0;
+  numeroMarchas: number = 5;
+  marchaAtual: number = 0;
+  velocidade: number = 0;
+  combustivel: number = 100;
+  ano: number = 0;
 }
--- index.ts
+++ index.ts
@@ -3,126 +3,138 @@ import { Veiculo } from "./Veiculo";
 
 const teclado = prompt();
 
-console.log('Criação de veículo');
+console.log("Criação de veículo");
 const carro: Veiculo = criaVeiculo();
 
-while(true){
-    console.log("########### MENU ###########");
-    console.log("1 - Acelerar");
-    console.log("2 - Frear");
-    console.log("3 - Subir marcha");
-    console.log("4 - Descer marcha");
-    console.log("5 - Imprimir dados do veículo");
-    console.log("6 - Abastecer");
-    console.log("0 - Sair");
+function criaVeiculo(): Veiculo {
+  const veiculo: Veiculo = new Veiculo();
+  veiculo.marca = teclado("Marca: ");
+  veiculo.modelo = teclado("Modelo: ");
+  veiculo.potencia = +teclado("Potência: ");
+  veiculo.rodas = +teclado("Número de rodas: ");
+  veiculo.numeroMarchas = +teclado("Número de marchas: ");
+  veiculo.ano = +teclado("Ano: ");
+  return veiculo;
+}
 
-    const opcao = +teclado('Escolha uma opção: ');
-    if (opcao === 0) break;
+while (true) {
+  console.clear();
+  console.log("########### MENU ###########");
+  console.log("1 - Acelerar");
+  console.log("2 - Frear");
+  console.log("3 - Subir marcha");
+  console.log("4 - Descer marcha");
+  console.log("5 - Imprimir dados do veículo");
+  console.log("6 - Abastecer");
+  console.log("0 - Sair");
 
-    switch (opcao) {
-        case 1:
-            acelerar(carro);
-            break;
-        case 2:
-            frear(carro);
-            break;
-        case 3:
-            subirMarcha(carro);
-            break;
-        case 5:
-            imprimir(carro);
-            break;
-        case 7:
-            abastecer(carro);
-        break;
+  const opcao = +teclado("Escolha uma opção: ");
+  if (opcao === 0) break;
 
-        default:
-            break;
-        case 4:
-            descerMarcha(carro);
-            break;
-    }
+  switch (opcao) {
+    case 1:
+      acelerar(carro);
+      break;
+    case 2:
+      frear(carro);
+      break;
+    case 3:
+      subirMarcha(carro);
+      break;
+    case 4:
+      descerMarcha(carro);
+      break;
+    case 5:
+      imprimir(carro);
+      break;
+    case 6:
+      abastecer(carro);
+      break;
+
+    default:
+      break;
+  }
+  teclado("ENTER para continuar.");
 }
 
 console.table(carro);
 
 function acelerar(veiculo: Veiculo): void {
-    if (veiculo.combustivel <= 0) {
-        console.log("Pane Seca! O veículo está sem combustível.");
-        veiculo.combustivel = 0;
-        return;
-    }
+  if (veiculo.combustivel <= 0) {
+    console.log("Pane Seca! O veículo está sem combustível.");
+    veiculo.combustivel = 0;
+    return;
+  }
 
-    if (veiculo.marchaAtual != 0) {
-        veiculo.velocidade += veiculo.potencia * 0.1;
-        
-        const gasto = veiculo.potencia * 0.05; 
-        veiculo.combustivel -= gasto;
+  if (veiculo.marchaAtual != 0) {
+    veiculo.velocidade += veiculo.potencia * 0.1;
 
-        if (veiculo.combustivel < 0) veiculo.combustivel = 0;
+    const gasto = veiculo.potencia * 0.05;
+    veiculo.combustivel -= gasto;
 
-        console.log(`Velocidade: ${veiculo.velocidade.toFixed(1)} km/h | Combustível: ${veiculo.combustivel.toFixed(1)}%`);
-    } else {
-        console.log("O carro está em ponto morto! Engate uma marcha.");
-    }
-}
+    if (veiculo.combustivel < 0) veiculo.combustivel = 0;
 
-function mudarMarcha(veiculo: Veiculo, novaMarcha: number): void { 
-    if (novaMarcha < -1 || novaMarcha > veiculo.numeroMarchas) {         
-        console.log("Marcha Inválida! Escolha entre -1 (ré) e " + veiculo.numeroMarchas);         
-        return;     }          veiculo.marchaAtual = novaMarcha;          
-        if (novaMarcha === -1) {         
-            console.log("Marcha em Ré");     
-        } 
-        else if (novaMarcha === 0) {        
-             console.log("Ponto Morto");     } else {        
-                 console.log("Marcha " + novaMarcha);     } }  
-                 
-
-
-function criaVeiculo(): Veiculo{
-    const veiculo: Veiculo = new Veiculo();
-    veiculo.marca = teclado('Marca: ');
-    veiculo.modelo = teclado('Modelo: ');
-    veiculo.potencia = +teclado('Potência: ');
-    veiculo.rodas = + teclado('Número de rodas: ')
-    veiculo.numeroMarchas = +teclado('Número de marchas: ');
-    veiculo.ano = +teclado('Ano: ');
-    return veiculo;
+    console.log(
+      `Velocidade: ${veiculo.velocidade.toFixed(1)} km/h | Combustível: ${veiculo.combustivel.toFixed(1)}%`,
+    );
+  } else {
+    console.log("O carro está em ponto morto! Engate uma marcha.");
+  }
 }
 
 function frear(veiculo: Veiculo): void {
-    if (veiculo.velocidade > 0) {
-        veiculo.velocidade -= veiculo.potencia * 0.1;
-        if (veiculo.velocidade < 0) veiculo.velocidade = 0;
-            console.log(`Velocidade atual: ${veiculo.velocidade.toFixed(1)}`);
-    } else {
-        console.log("O veículo já está parado.");
-    }
-}
-
-function imprimir(veiculo: Veiculo): void {
-    console.log("Ano: "+veiculo.ano);
-    console.log("Marca: "+veiculo.marca);
-    console.log("Modelo: "+veiculo.modelo);
-    console.log("Rodas: "+veiculo.rodas);
+  if (veiculo.velocidade > 0) {
+    veiculo.velocidade -= veiculo.potencia * 0.1;
+    if (veiculo.velocidade < 0) veiculo.velocidade = 0;
+    console.log(`Velocidade atual: ${veiculo.velocidade.toFixed(1)}`);
+  } else {
+    console.log("O veículo já está parado.");
+  }
 }
 
 function subirMarcha(veiculo: Veiculo): void {
-    if (veiculo.marchaAtual < veiculo.numeroMarchas) {
-        veiculo.marchaAtual++;
-        console.log(`Marcha atual: ${veiculo.marchaAtual}`);
-    } else {
-        console.log("Já está na marcha máxima.");
-    }
+  if (veiculo.marchaAtual < veiculo.numeroMarchas) {
+    veiculo.marchaAtual++;
+    console.log(`Marcha atual: ${veiculo.marchaAtual}`);
+  } else {
+    console.log("Já está na marcha máxima.");
+  }
+}
+
+function descerMarcha({ marchaAtual, numeroMarchas }: Veiculo): void {
+  if (marchaAtual < -1) {
+    console.log("Marcha Inválida! Escolha entre -1 (ré) e " + numeroMarchas);
+    return;
+  }
+  marchaAtual--;
+  console.log(`Marcha atual: ${marchaAtual}`);
+}
+
+function mudarMarcha(veiculo: Veiculo, novaMarcha: number): void {
+  if (novaMarcha < -1 || novaMarcha > veiculo.numeroMarchas) {
+    console.log("Marcha Inválida! Escolha entre -1 (ré) e " + veiculo.numeroMarchas);
+    return;
+  }
+  veiculo.marchaAtual = novaMarcha;
+  if (novaMarcha === -1) {
+    console.log("Marcha em Ré");
+  } else if (novaMarcha === 0) {
+    console.log("Ponto Morto");
+  } else {
+    console.log("Marcha " + novaMarcha);
+  }
+}
+
+function imprimir(veiculo: Veiculo): void {
+  console.table(veiculo);
 }
 
 function abastecer(veiculo: Veiculo): void {
-    if (veiculo.velocidade > 0) {
-        console.log("Segurança em primeiro lugar! Pare o carro para abastecer.");
-        return;
-    }
-    
-    veiculo.combustivel = 100;
-    console.log("Depósito cheio! Pode seguir viagem.");
+  if (veiculo.velocidade > 0) {
+    console.log("Segurança em primeiro lugar! Pare o carro para abastecer.");
+    return;
+  }
+
+  veiculo.combustivel = 100;
+  console.log("Depósito cheio! Pode seguir viagem.");
 }
